4f25a1488d88ae3b60420a5360c52ad660dff13a
Chibuzor <chibuzormekalam@gmail.com>
2025-08-01 13:50:03 +0100

    created uploads file

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,10 +1,11 @@
 import { getUsers, registerUsers } from "../controller/userController.js";
 import express from "express";
+import uploads from "../middleware/uploads.js";
 const userRouter = express.Router();
 
 
 
 userRouter.get("/getUsers", getUsers);
-userRouter.post("/registerUsers", registerUsers);
+userRouter.post("/registerUsers", uploads.single("image"), registerUsers);
 
 export  {userRouter};
