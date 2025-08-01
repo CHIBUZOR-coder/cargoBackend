52a35e1412cc2f1a1c4696cc92a5a5401369d389
Chibuzor <chibuzormekalam@gmail.com>
2025-08-01 13:14:54 +0100

    adding cloudinary features

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
 import pool from "../db.js"; // ✅ Triggers pool setup
 import dotenv from "dotenv";
 import bcrypt, { compare } from "bcrypt";
+import { cloudinary } from "../config/cloudinary.js";
 dotenv.config();
 
 export const getUsers = async (req, res) => {
@@ -85,6 +86,34 @@ export const registerUsers = async (req, res) => {
         .status(400)
         .json({ success: false, message: "Password does not match" });
 
+    let imageUrl;
+    if (!req.file) {
+      return res.status(400).json({ message: "No file provided" });
+    }
+
+    if (req.file) {
+      console.log("Starting image upload...");
+      // console.log("File buffer:", req.file.buffer);
+      const allowedMimeTypes = ["image/jpeg", "image/png", "image/webp"];
+      if (!allowedMimeTypes.includes(req.file.mimetype)) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Invalid image format" });
+      }
+      try {
+        // Use async/await to handle the image upload process
+        imageUrl = await uploadImageToCloudinary(req.file.buffer);
+
+        // After the upload completes, log the image URL
+        console.log("Image URL after upload:", imageUrl);
+      } catch (error) {
+        console.error("Error during upload:", error);
+        return res
+          .status(500)
+          .json({ success: false, message: "Image upload failed" });
+      }
+    }
+
     //crate a bycrypt salt
     const salt = await bcrypt.genSalt(10);
 
@@ -103,23 +132,43 @@ export const registerUsers = async (req, res) => {
 
     const newUser = await pool.query(
       "INSERT INTO users (firstname, lastname, email, phone, address, password, confirmpassword) VALUES($1, $2, $3, $4, $5, $6, $7) Returning * ",
-      [
-        firstname,
-        lastname,
-        email,
-        phone,
-        address,
-        hashedPassword,
-        confirmpassword,
-      ]
+      [firstname, lastname, email, phone, address, hashedPassword, null]
     );
 
     if (newUser.rowCount > 0) {
-      return res.status(201).json({ success: false, message:"User Registered Sucessfully !" });
+      return res
+        .status(201)
+        .json({ success: true, message: "User Registered Sucessfully !" });
     }
   } catch (error) {
     console.log(error.message);
 
     return res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+
+const uploadImageToCloudinary = async (fileBuffer) => {
+  try {
+    const uploadPromise = new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: "image", folder: "CargoMerge" },
+          (error, result) => {
+            if (error) {
+              return reject(error);
+            }
+            resolve(result);
+          }
+        )
+        .end(fileBuffer);
+    });
+
+    const result = await uploadPromise;
+    console.log("Upload successful:", result.secure_url);
+    return result.secure_url;
+  } catch (error) {
+    console.error("Upload failed:", error);
+    throw new Error("Image upload failed");
   }
 };
