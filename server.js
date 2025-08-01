33213171db911f3a82fe4e753e1b5361c4d7b577
Chibuzor <chibuzormekalam@gmail.com>
2025-08-01 03:20:24 +0100

    fixing router for user registration

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import { userRouter } from "./Router/userRouter.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use("/", userRouter)
+
 app.listen(port, () => {
   console.log(`Connected sussesfully at ${port}`);
 });
