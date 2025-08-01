37564b652b549a76f22d3a80001abe9ba6ec4fcf
Chibuzor <chibuzormekalam@gmail.com>
2025-08-01 03:44:11 +0100

    added body parser2

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ import { userRouter } from "./Router/userRouter.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-app.use("/", userRouter)
 app.use(express.json()); // <-- Parses JSON bodies
 app.use(express.urlencoded({ extended: true })); 
+
+app.use("/", userRouter)
 
 app.listen(port, () => {
   console.log(`Connected sussesfully at ${port}`);
