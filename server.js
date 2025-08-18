ac131735cbc56da824abaeb5c4a0d1db9840bd53
Chibuzor <chibuzormekalam@gmail.com>
2025-08-18 02:29:06 +0100

    Add userLogin functionality4

--- server.js
+++ server.js
@@ -20,13 +20,34 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { userRouter } from "./Router/userRouter.js";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+// parse cookies
+app.use(cookieParser());
+const allowedOrigins = [
+  "https://cargo-merge.vercel.app/", // Deployed React frontend
+  "http://localhost:5173", // Local React dev
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl/postman)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("Blocked by CORS: ", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.use(cors());
 
 // These parsers are fine for normal requests
 app.use(express.json());
